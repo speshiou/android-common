56d91de6c732cb62d71e7378472c849f3b6a30fc
pshaung <speshiou@gmail.com>
2019-04-29 16:17:01 +0800

    Initialize FB ads in base application

--- library/src/main/java/com/speshiou/android/common/ui/BaseApplication.kt
+++ library/src/main/java/com/speshiou/android/common/ui/BaseApplication.kt
@@ -1,13 +1,20 @@
 package com.speshiou.android.common.ui
 
 import android.app.Application
+import android.webkit.WebView
+import com.facebook.ads.AudienceNetworkAds
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.speshiou.android.common.BuildConfig
 
 open class BaseApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
         firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        if (BuildConfig.DEBUG) {
+            WebView.setWebContentsDebuggingEnabled(true)
+        }
+        AudienceNetworkAds.initialize(this)
     }
 
     companion object {
