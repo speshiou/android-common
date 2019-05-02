bc9e5eae632d14a85e103f732fa147a610fb8ada
pshaung <speshiou@gmail.com>
2019-05-02 14:47:34 +0800

    Make security API static

--- library/src/main/java/com/speshiou/android/common/security/SecurityUtils.kt
+++ library/src/main/java/com/speshiou/android/common/security/SecurityUtils.kt
@@ -2,30 +2,32 @@ package com.speshiou.android.common.security
 
 import java.security.NoSuchAlgorithmException
 
-object SecurityUtils {
-    fun md5(s: String): String {
-        val MD5 = "MD5"
-        try {
-            // Create MD5 Hash
-            val digest = java.security.MessageDigest
-                    .getInstance(MD5)
-            digest.update(s.toByteArray())
-            val messageDigest = digest.digest()
+class SecurityUtils {
+    companion object {
+        fun md5(s: String): String {
+            val MD5 = "MD5"
+            try {
+                // Create MD5 Hash
+                val digest = java.security.MessageDigest
+                        .getInstance(MD5)
+                digest.update(s.toByteArray())
+                val messageDigest = digest.digest()
 
-            // Create Hex String
-            val hexString = StringBuilder()
-            for (aMessageDigest in messageDigest) {
-                var h = Integer.toHexString(0xFF and aMessageDigest.toInt())
-                while (h.length < 2)
-                    h = "0" + h
-                hexString.append(h)
+                // Create Hex String
+                val hexString = StringBuilder()
+                for (aMessageDigest in messageDigest) {
+                    var h = Integer.toHexString(0xFF and aMessageDigest.toInt())
+                    while (h.length < 2)
+                        h = "0" + h
+                    hexString.append(h)
+                }
+                return hexString.toString()
+
+            } catch (e: NoSuchAlgorithmException) {
+                e.printStackTrace()
             }
-            return hexString.toString()
 
-        } catch (e: NoSuchAlgorithmException) {
-            e.printStackTrace()
+            return ""
         }
-
-        return ""
     }
 }
