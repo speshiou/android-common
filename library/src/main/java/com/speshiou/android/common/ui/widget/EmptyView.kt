c6105eec17a8a21fa7605a502b6a56a5b1f216cf
pshaung <speshiou@gmail.com>
2021-04-06 15:00:49 +0800

    Replace Kotlin synthetics with view binding

--- library/src/main/java/com/speshiou/android/common/ui/widget/EmptyView.kt
+++ library/src/main/java/com/speshiou/android/common/ui/widget/EmptyView.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
 import com.speshiou.android.common.R
-import kotlinx.android.synthetic.main.empty_view.view.*
+import com.speshiou.android.common.databinding.EmptyViewBinding
 import kotlin.properties.Delegates
 
 class EmptyView(context: Context, attrs: AttributeSet): FrameLayout(context, attrs) {
+
+    private val binding: EmptyViewBinding
+
     init {
         LayoutInflater.from(context).inflate(R.layout.empty_view, this, true)
-        action_button.visibility = View.GONE
+        binding = EmptyViewBinding.inflate(LayoutInflater.from(context), this)
+        binding.actionButton.visibility = View.GONE
 
         init(context, attrs)
     }
@@ -26,9 +30,9 @@ class EmptyView(context: Context, attrs: AttributeSet): FrameLayout(context, att
             )
             try {
                 val d = a.getDrawable(R.styleable.EmptyView_emptyIcon)
-                icon.setImageDrawable(d)
+                binding.icon.setImageDrawable(d)
                 val text = a.getString(R.styleable.EmptyView_emptyText)
-                prompt_text.text = text
+                binding.promptText.text = text
             } finally { // release the TypedArray so that it can be reused.
                 a.recycle()
             }
@@ -37,25 +41,25 @@ class EmptyView(context: Context, attrs: AttributeSet): FrameLayout(context, att
 
     var text: CharSequence by Delegates.observable<CharSequence>("") {
         property, oldValue, newValue ->
-        prompt_text.text = newValue
+        binding.promptText.text = newValue
     }
 
     var iconResId: Int by Delegates.observable(-1) {
         property, oldValue, newValue ->
         if (newValue > 0) {
-            icon.visibility = View.VISIBLE
-            icon.setImageResource(iconResId)
+            binding.icon.visibility = View.VISIBLE
+            binding.icon.setImageResource(iconResId)
         } else {
-            icon.visibility = View.GONE
+            binding.icon.visibility = View.GONE
         }
     }
 
     val actionButton by lazy {
-        action_button
+        binding.actionButton
     }
 
     var actionButtonEnabled: Boolean by Delegates.observable(false) {
         property, oldValue, newValue ->
-        action_button.visibility = if (newValue) View.VISIBLE else View.GONE
+        binding.actionButton.visibility = if (newValue) View.VISIBLE else View.GONE
     }
 }
