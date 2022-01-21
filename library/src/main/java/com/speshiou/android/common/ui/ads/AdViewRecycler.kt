a7c683f9e674fda15189d4f90538b6b704aa0c47
pshaung <speshiou@gmail.com>
2022-01-21 13:56:18 +0800

    Remove AFS support

--- library/src/main/java/com/speshiou/android/common/ui/ads/AdViewRecycler.kt
+++ library/src/main/java/com/speshiou/android/common/ui/ads/AdViewRecycler.kt
@@ -58,9 +58,6 @@ class AdViewRecycler(private val mContext: Context) {
         if (reusedTask is LoadCSATask) {
             reusedTask.keyword = keyword
         }
-        if (reusedTask is LoadAFSNativeAdTask) {
-            reusedTask.keyword = keyword
-        }
         return reusedTask
     }
 
